4be23731d611c038b2a242ac4bfb42296d44a3bd
Fanaar <fanaarrashid@gmail.com>
2026-03-22 12:18:18 +0100

    Opschonen Scripts

--- Potluck!/Assets/Scripts/Rolling Dolma Scripts/QuestionData.cs
+++ Potluck!/Assets/Scripts/Rolling Dolma Scripts/QuestionData.cs
@@ -4,8 +4,4 @@ using UnityEngine;
 public class QuestionData : ScriptableObject
 {
     public string questionStart;
-    public string questionEnding;
-
-    [TextArea(3, 6)]
-    public string motherResponse;
 }
