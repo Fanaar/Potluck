065b0b9e2dae4f981136b59b6c15a58bdfd9041d
Fanaar <fanaarrashid@gmail.com>
2026-03-23 10:59:52 +0100

    Dadelboom scene

---
+++ Potluck!/Assets/Scripts/Rolling Dolma Scripts/Dadelboom/FadeOnTrigger.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOnTrigger : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private bool isFading = false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Debug.Log("FadeOnTrigger gestart op: " + gameObject.name);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger geraakt met: " + other.name);
+
+        if (other.CompareTag("Target"))
+        {
+            Debug.Log("Target geraakt → start fade");
+
+            if (!isFading)
+            {
+                StartCoroutine(FadeOut());
+            }
+        }
+        else
+        {
+            Debug.Log("Geen juiste tag: " + other.tag);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        isFading = true;
+
+        float duration = 1f;
+        float time = 0f;
+        Color startColor = sr.color;
+
+        Debug.Log("Fade gestart");
+
+        while (time < duration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, time / duration);
+            sr.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        sr.color = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        Debug.Log("Fade klaar");
+    }
+}
---
+++ Potluck!/Assets/Scripts/Rolling Dolma Scripts/Dadelboom/MouseFollower.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    public LayerMask wallLayer;
+    public float radius = 0.3f;
+    public float smoothSpeed = 15f;
+
+    public Transform startPoint; // 👈 deze toevoegen
+
+    void Start()
+    {
+        if (startPoint != null)
+        {
+            transform.position = startPoint.position;
+        }
+    }
+
+    void Update()
+    {
+        Vector2 currentPos = transform.position;
+        Vector2 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = targetPos - currentPos;
+        Vector2 desiredMove = direction * smoothSpeed * Time.deltaTime;
+
+        Vector2 moveX = new Vector2(desiredMove.x, 0);
+        if (!IsBlocked(currentPos + moveX))
+        {
+            currentPos += moveX;
+        }
+
+        Vector2 moveY = new Vector2(0, desiredMove.y);
+        if (!IsBlocked(currentPos + moveY))
+        {
+            currentPos += moveY;
+        }
+
+        transform.position = currentPos;
+    }
+
+    bool IsBlocked(Vector2 position)
+    {
+        return Physics2D.OverlapCircle(position, radius, wallLayer);
+    }
+}
